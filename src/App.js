9374514ee430ad8f4675c64c42da932a1387d25a
Or Hasson <hassonor@gmail.com>
2021-10-30 23:36:13 +0300

    1st commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import {useState, useEffect, useCallback } from "react";
+import {BiCalendar, BiTrash} from "react-icons/bi";
+import Search from "./components/Search";
+import AddAppointment from "./components/AddAppointment";
+import AppointmentInfo from "./components/AppointmentInfo";
+
+function App() {
+    let [appointmentList, setAppointmentList] = useState([]);
+
+    const fetchData = useCallback(()=>{
+        fetch('./data.json')
+            .then(response => response.json())
+            .then(data => {
+                setAppointmentList(data);
+            })
+            .catch(err => console.log(err));
+    }, []);
+
+    useEffect(()=>{
+        fetchData();
+    },[fetchData])
+
+    return (
+        <div className="App container mx-auto mt-3 font-thin">
+            <h1 className="text-5xl mb-3"><BiCalendar className="inline-block text-red-400"/>Your Appointments</h1>
+            <AddAppointment/>
+            <Search/>
+
+            <ul className="divide-y divide-gray-200">
+                {appointmentList.map(appointment => (
+                    <AppointmentInfo key={appointment.id}
+                                     appointment={appointment}
+                                     onDeleteAppointment={ appointmentId =>
+                                     setAppointmentList(
+                                         appointmentList.filter(appointment => appointment.id !== appointmentId))}/>
+                ))
+                }
+            </ul>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/Search.js
@@ -0,0 +1,57 @@
+import { BiSearch, BiCaretDown, BiCheck } from "react-icons/bi"
+import {useState} from "react";
+
+const DropDown = ({toggle}) => {
+    if(!toggle) {
+        return null;
+    }
+    return (
+        <div className="origin-top-right absolute right-0 mt-2 w-56
+      rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
+            <div className="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
+                <div
+                    className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 flex justify-between cursor-pointer"
+                    role="menuitem">Pet Name <BiCheck /></div>
+                <div
+                    className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 flex justify-between cursor-pointer"
+                    role="menuitem">Owner Name  <BiCheck /></div>
+                <div
+                    className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 flex justify-between cursor-pointer"
+                    role="menuitem">Date <BiCheck /></div>
+                <div
+                    className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 flex justify-between cursor-pointer border-gray-1 border-t-2"
+                    role="menuitem">Asc <BiCheck /></div>
+                <div
+                    className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 flex justify-between cursor-pointer"
+                    role="menuitem">Desc <BiCheck /></div>
+            </div>
+        </div>
+    )
+}
+
+const Search = () => {
+    let [toggleSort, setToggleSort] = useState(false);
+    return (
+        <div className="py-5">
+            <div className="mt-1 relative rounded-md shadow-sm">
+                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                    <BiSearch />
+                    <label htmlFor="query" className="sr-only" />
+                </div>
+                <input type="text" name="query" id="query" value=""
+                       className="pl-8 rounded-md focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300" placeholder="Search" />
+                <div className="absolute inset-y-0 right-0 flex items-center">
+                    <div>
+                        <button type="button" onClick={()=> setToggleSort(!toggleSort)}
+                                className="justify-center px-4 py-2 bg-blue-400 border-2 border-blue-400 text-sm text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 flex items-center" id="options-menu" aria-haspopup="true" aria-expanded="true">
+                            Sort By <BiCaretDown className="ml-2" />
+                        </button>
+                        <DropDown toggle={toggleSort} />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Search
